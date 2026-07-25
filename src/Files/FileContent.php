cbac4d1ec8d3f4f96ad9090f9ad039a514c6b9cc
higaki-takanori <garoumare@gmail.com>
2026-07-25 20:59:59 +0900

    feat: ファイルの中身 のクラス作成

---
+++ src/Files/FileContent.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phigaki\Tar\Files;
+
+use Phigaki\Tar\Fundamentals\PadResolver;
+
+final readonly class FileContent
+{
+    public const int BYTE_LENGTH = 512;
+
+    private function __construct(
+        public string $content,
+    ) {
+    }
+
+    public static function from(string $path): self
+    {
+        if (! is_file($path) || ! is_readable($path)) {
+            throw new \RuntimeException("読み取り不可のファイルです。: {$path}");
+        }
+
+        $content = file_get_contents($path);
+        if ($content === false) {
+            throw new \RuntimeException("ファイルの読み取りに失敗しました。: {$path}");
+        }
+
+        return new self(content: $content);
+    }
+
+    public function bytes(): string
+    {
+        return PadResolver::padNull($this->content, self::BYTE_LENGTH);
+    }
+}
