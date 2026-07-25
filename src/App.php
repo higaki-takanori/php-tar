722f94c8f786c5eb13bbc4daa7fae4472ede1641
higaki-takanori <garoumare@gmail.com>
2026-07-25 21:10:27 +0900

    feat: 2ファイルを対象にTarを保存する

--- src/App.php
+++ src/App.php
@@ -8,9 +8,10 @@ final readonly class App
 {
     public static function main(): void
     {
-        $filePath = './files/sample.txt';
+        $filePath1 = './files/sample.txt';
+        $filePath2 = './files/sample2.txt';
 
-        $tar = Tar::from([$filePath]);
+        $tar = Tar::from([$filePath1, $filePath2]);
         $tar->save('sample.tar');
     }
 }
