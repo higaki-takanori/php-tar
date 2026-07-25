7bbce86f99c147045594284405956b8765c10f86
higaki-takanori <garoumare@gmail.com>
2026-07-25 21:09:30 +0900

    feat: Tarを保存する

--- src/App.php
+++ src/App.php
@@ -2,8 +2,7 @@
 
 namespace Phigaki\Tar;
 
-use Phigaki\Tar\Files\FileContent;
-use Phigaki\Tar\Files\Headers\Header;
+use Phigaki\Tar\Files\Tar;
 
 final readonly class App
 {
@@ -11,12 +10,7 @@ final readonly class App
     {
         $filePath = './files/sample.txt';
 
-        $header = Header::create($filePath);
-        $content = FileContent::from($filePath);
-
-        var_dump($header);
-        var_dump($header->bytes());
-        var_dump($content);
-        var_dump($content->bytes());
+        $tar = Tar::from([$filePath]);
+        $tar->save('sample.tar');
     }
 }
