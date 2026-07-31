bc4e4185db8fb4f2733317828164d5ddd2b2c6bc
higaki-takanori <garoumare@gmail.com>
2026-07-31 18:21:18 +0900

    feat: tarballにするファイルを引数から指定できる

--- src/App.php
+++ src/App.php
@@ -2,17 +2,22 @@
 
 namespace Phigaki\Tar;
 
+use Exception;
 use Phigaki\Tar\Files\Tar;
+use UnexpectedValueException;
 
 final readonly class App
 {
-    public static function main(): void
+    /** @param string[] $argv */
+    public static function main(int $argc, array $argv): void
     {
-        $filePath1 = './files/sample.txt';
-        $filePath2 = './files/sample2.txt';
-        $filePath3 = './files/directory/';
+        $filePaths = match ($argc) {
+            0 => throw new UnexpectedValueException('想定し得ない値が入力されました。'),
+            1 => throw new Exception('tarball にするファイルを指定してください。'),
+            default => array_slice($argv, 1),
+        };
 
-        $tar = Tar::from([$filePath1, $filePath2, $filePath3]);
+        $tar = Tar::from($filePaths);
         $tar->save('sample.php.tar');
     }
 }
